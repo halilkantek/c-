c4993931433f2f5c0a13fc1fffd5bd5f6d1615f1
halilkantek <61408998+halilkantek@users.noreply.github.com>
2020-02-27 10:40:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,117 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2112
+namespace ConsoleApp2
 {
+    class ogrenci
+    {
+
+        public string ogrenciAdi = "";
+        int vize;
+        int final;
+        public bool gectiMi
+        {
+            get
+            {
+                return ortalama > 49 ? true : false;
+            }
+        }
+        public double ortalama
+        {
+            get
+            {
+                //Random rnd = new Random();
+                //return rnd.NextDouble()*100;
+                return vize * 0.4 + final * 0.6;
+            }
+        }
+
+        public int sVize
+        {
+            set
+            {
+                if (value < 101 && value > 1)
+                {
+                    vize = value;
+                }
+
+            }
+            get
+            {
+                return vize;
+            }
+        }
+        public int sFinal
+        {
+            set
+            {
+                if (value < 101 && value > 1)
+                {
+                    final = value;
+                }
+
+            }
+            get
+            {
+                return final;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //ögrenci h1 = new ögrenci();
-            ögrenci[] ogrdizi = new ögrenci[10];
-             
-           
-           
-            string menü;
-            int ogrsay = 0;
+            List<ogrenci> ogrList = new List<ogrenci>();
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("1- ogrenci ekleme:");
-            Console.WriteLine("2- ogr noya göre ders ekleme:");
-            Console.WriteLine("3- tüm verileri listele");
-            Console.WriteLine("4-uygulmadan cik: ");
-        hk:
-            ogrdizi[ogrsay] = new ögrenci();
-            Console.Write("isleminiz:");
-            menü = Console.ReadLine();
- //           h1.ad = "halil";
- //           h1.soyad = "kantek";
- //           h1.ogrno = "364";
- //           h1.alinandersler[0] = new ders();
- //           h1.alinandersler[0].dersadi = "proglama";
- //           h1.alinandersler[0].derskod = "1515";
- //           h1.alinandersler[0].derssaati = "3";
- //           Console.WriteLine(h1.ad + "   " + h1.soyad + "    " + h1.ogrno + "    ");
- //           Console.WriteLine(h1.alinandersler[0].dersadi + " " + h1.alinandersler[0].derskod + " " +
- //h1.alinandersler[0].derssaati);
+            Random rnd = new Random();
 
- //           Console.ReadLine();
- //       }
+            for (int i = 0; i < 1000; i++)
+            {
+                ogrenci x = new ogrenci();
+                x.sVize = rnd.Next(0, 100);
+                x.sFinal = rnd.Next(0, 100);
+
+                ogrList.Add(x);
+            }
+
+            List<ogrenci> yeniListe = ogrList.Where(s => s.sVize > 50).ToList();
 
 
 
-       switch (menü)
+            Console.ReadKey();
 
-       {
-           case "1":
-
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write("ad giriniz:");
-                    ogrdizi[ogrsay].ad=(Console.ReadLine());
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write("soyad gir:");
-                    ogrdizi[ogrsay].soyad = Console.ReadLine();
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write("ogr no gir");
-                    ogrdizi[ogrsay].ogrno = Console.ReadLine();
-                        ogrsay++;
-          
-                  break;
-      case "2":
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ders eklemek istedğiniz ögrenci no");
-                    string no = Console.ReadLine();
-                    int say = 0;
-                    foreach (var item in ogrdizi)
-                    {
-                        if (item.ogrno == no)
-                            break;
-                            say++;
-                    }
-                    int ogrders_sayasi = ogrdizi[say].derssay;
-                    ogrdizi[say].ogrders[ogrdizi[say].derssay] = new ders();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ders adi gir:");
-                    ogrdizi[say].ogrders[ogrdizi[say].derssay].dersadi = Console.ReadLine();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ders kodu gir:");
-                    ogrdizi[say].ogrders[ogrdizi[say].derssay].derskod = Console.ReadLine();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ders saati:");
-                    ogrdizi[say].ogrders[ogrdizi[say].derssay].derssaati = Console.ReadLine();
-                    ogrdizi[say].derssay++;
-
-                    break;
-                case "3":
-                    for (int i = 0; i < ogrsay; i++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.WriteLine("ogrenci adı= " + ogrdizi[i].ad + "\nogrenci soyadı= " + ogrdizi[i].soyad+ "\nogrenci no= " + ogrdizi[i].ogrno );
-                        Console.WriteLine("-------------------------------------------------------------");
-
-                        int a = ogrdizi[i].derssay;
-                        for (int g = 0; g < a; g++)
-                        {
-                            Console.WriteLine("\ndersin adı= " + ogrdizi[i].ogrders[g].dersadi + "\ndersin kodu= " + ogrdizi[i].ogrders[g].derskod + "\nderssaati=" + ogrdizi[i].ogrders[g].derssaati  );
-                            Console.WriteLine("********************************************");
-                        }
-                    }
-
-                    
-                    
-
-                            
-            
-                    break;
-                case "4":
-                    Environment.Exit(0);
-                    break;
-                    
-          }
-          goto hk;
-          Console.ReadLine();
-
-     }
+        }
     }
+
 }
+
